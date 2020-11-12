539c0b8babee695f35378b1065c48bac6f3d5f3e
dawg <dawid.golebiewski@tuhh.de>
2020-11-12 01:36:25 +0100

    GTFO useless coder of this shit starter

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,7 +6,7 @@
 
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
-        <nuxt-link to="https://medium.com/@dawid.golebiewski">
+        <nuxt-link :to="https://medium.com/@dawid.golebiewski">
         <img
           :src="robotech"
           alt="Hero"
